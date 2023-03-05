a8191d683457a1b0a1493d7207feb8d3047a730e
Opio Maxwell <opiomaxwell11122@gmail.com>
2023-03-05 23:10:08 +0300

    define typography

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div className='container'>
+            <p>nav bar peakoko</p>
+        </div>
+    );
+};
+
+export default Navbar;
